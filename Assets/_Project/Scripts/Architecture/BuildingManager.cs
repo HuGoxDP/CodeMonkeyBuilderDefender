bb6d303d0b75ff541f662a8dd7f1bc9d67dcac0b
HugoxDP <sasha.maidanika@gmail.com>
2025-06-27 20:59:47 +0300

    feat(meta): Add new metadata files for project assets
    - Introduced new .meta files for various assets
    - Updated file format version and GUIDs
    - Ensured folder asset settings are correctly configured

---
+++ Assets/_Project/Scripts/Architecture/BuildingManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Architecture
+{
+    public class BuildingManager : MonoBehaviour
+    {
+        private void Update()
+        {
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Architecture/InputReader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Project.Scripts.Architecture
+{
+    public class InputReader : MonoBehaviour
+    {
+        private PlayerInput _playerInput;
+        private InputAction _pointPosition;
+        public int Aware;
+        private const int Awares = 5;
+        [SerializeField] private string _testUp;
+        [SerializeField] public string Test;
+        protected int TestProt;
+        private static int s_dfs;
+
+
+        public Vector2 PointPosition => _pointPosition.ReadValue<Vector2>();
+
+        private void Start()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+
+        }
+    }
+}
