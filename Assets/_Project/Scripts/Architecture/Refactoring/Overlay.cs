b9c7fcc6e1d91bf598d57c8553b9c2c5937faa72
HugoxDP <sasha.maidanika@gmail.com>
2025-08-08 20:45:12 +0300

    refactor(overlay): Simplify overlay pool management and release logic
    - Remove the private overlay pool variable
    - Streamline the OnDestroy method to call Release directly
    - Update UpdateData method to call Release before updating position
    - Make Release an abstract method to enforce implementation in derived classes

--- Assets/_Project/Scripts/Architecture/Refactoring/Overlay.cs
+++ Assets/_Project/Scripts/Architecture/Refactoring/Overlay.cs
@@ -1,40 +1,24 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.Pool;
+﻿using UnityEngine;
 
 namespace _Project.Scripts.Architecture.Refactoring
 {
     public abstract class Overlay<TOverlayData> : MonoBehaviour, IOverlay<TOverlayData>
         where TOverlayData : IOverlayData
     {
-        private IObjectPool<IOverlay<TOverlayData>> _overlayPool;
-
         protected virtual void OnDestroy()
         {
-            if (_overlayPool != null)
-            {
-                var pool = _overlayPool;
-                _overlayPool = null;
-                pool.Release(this);
-            }
+            Release();
         }
 
         public GameObject GameObject => gameObject;
         public bool IsActive => gameObject.activeInHierarchy;
 
-        public void InitializePool(IObjectPool<IOverlay<TOverlayData>> pool)
-        {
-            _overlayPool = pool ?? throw new ArgumentNullException(nameof(pool));
-        }
-
         public virtual void UpdateData(TOverlayData data)
         {
+            Release();
             transform.position = data.Position;
         }
 
-        public virtual void Release()
-        {
-            _overlayPool.Release(this);
-        }
+        public abstract void Release();
     }
 }
